1eae1b02e8df313a1e5f356dbb62477d034c36be
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-03-27 15:24:08 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,13 +1,17 @@
-import hxz from 'hxz-api'
-
-let handler = async(m,{text, conn, usedPrefix, command}) => {
-if (!text) throw `Use example ${usedPrefix}${command}\nhttps://vt.tiktok.com/ZGJBtcsDq/`
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, null, m)
+import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, usedprefix, command }) => {
+    if (!args[0]) throw `use example ${usedprefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
+    const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
+    if (!url) throw 'can\'t download video!'
+    conn.sendfile(m.chat, url, 'tiktok.mp4', `
+🔗 *url:* ${url}
+🧏 *nickname:* ${nickname}${description ? `🖹 *description:* ${description}` : ''}
+`.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok|tt|tiktoknowm)$/i
 
-export default handler
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+export default handler 
