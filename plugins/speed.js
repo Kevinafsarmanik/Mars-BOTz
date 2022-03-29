137fce62d5f05357d8ab70d1fd8b8a7493488c86
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-03-29 21:47:51 +0700

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -62,7 +62,7 @@ ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type =>
 _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `
-  conn.sendButton(m.chat, txt , wm, [['Back To Menu', '.menu'], ['Donate', '.donasi']], m)
+conn.sendButton(m.chat, txt , wm, [['Back To Menu', '.menu'], ['Donate', '.donasi']], m)
 }
 handler.help = ['ping', 'speed']
 handler.tags = ['info', 'tools']
