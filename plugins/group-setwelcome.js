a39ac9f8b77215169895cb9a3d1dfe4933f4be62
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-03-30 20:53:47 +0700

    Create group-setwelcome.js

---
+++ plugins/group-setwelcome.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+  if (text) {
+    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    db.data.chats[m.chat].sWelcome = text
+    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
+  } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} selamat datang @user digrup @subject\n\n@desc`
+}
+handler.help = ['setwelcome <teks>']
+handler.tags = ['group']
+handler.command = /^setwelcome$/i
+
+export default handler
