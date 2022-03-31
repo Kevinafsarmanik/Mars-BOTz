5cdcdaebc696a55a034865428406d4ca161e978d
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-03-31 17:26:19 +0700

    Update random-darkjoke.js

--- plugins/random-darkjoke.js
+++ plugins/random-darkjoke.js
@@ -1,12 +1,16 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let url = global.API('lolhuman', '/api/meme/darkjoke', {}, 'apikey')
-  await conn.sendButton(m.chat, 'Nih banh', wm, url, [
-  ['Next', `${usedPrefix+command}`]
-], m, 0, { thumbnail: await(await fetch(url)).buffer() })
+let handler = async (m, { conn }) => {
+  let res = global.API('lolhuman', '/api/meme/darkjoke', {}, 'apikey')
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (!json.image) throw 'Err'
+  let thumbnail = await (await fetch(json.image)).buffer()
+  conn.sendFile(m.chat, json.img, null, null, m, 0, { thumbnail })
 }
+
 handler.help = ['darkjoke']
-handler.tags = ['random']
-handler.command = /^(darkjoke|darkjokes)$/i
+handler.tags = ['internet']
+
+handler.command = /^(darkjoke)$/i
 
 export default handler
