98ca53b5f5abc402bf814209e13bf71f7f2b146a
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-03-31 17:30:57 +0700

    Update random-darkjoke.js

--- plugins/random-darkjoke.js
+++ plugins/random-darkjoke.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 let res = global.API('lolhuman', '/api/meme/darkjoke', {}, 'apikey')
-  await conn.sendButton(m.chat, 'Nih banh', wm, url, [
+  await conn.sendButton(m.chat, 'Nih banh', wm, res, [
   ['Next', `${usedPrefix+command}`]
 ], m, 0, { thumbnail: await(await fetch(res)).buffer() })
 }
