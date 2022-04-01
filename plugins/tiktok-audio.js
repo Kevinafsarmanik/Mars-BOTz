9d0f1417428e8b9f98977704f290cd6d1921d472
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-01 08:18:31 +0700

    Update tiktok-audio.js

--- plugins/tiktok-audio.js
+++ plugins/tiktok-audio.js
@@ -9,10 +9,7 @@ let handler = async (m, { conn, args, usedprefix, command }) => {
     if (!url) throw 'can\'t download video!'
 let f = await(await fetch(url)).buffer()
 let audio = await toAudio(f, 'mp4')
-    conn.sendFile(m.chat, audio.data, 'tiktok.mp3', `
-*Nickname:* ${nickname}
-*Description:* ${description}
-`.trim(), m, null,  {mimetype:'audio/mp4'})
+    await conn.sendMessage(m.chat, { document: { url: url}, mimetype: 'audio/mpeg', fileName: `tiktok.mp3`}, {quoted: m})
 }
 handler.command = /^(tiktokmusik|ttmusik)$/i
 
