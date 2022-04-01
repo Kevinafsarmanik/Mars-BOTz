32ea23edffc481cc02c9a5d56a88bb548dacb796
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-01 09:25:40 +0700

    Create fun-react.js

---
+++ plugins/fun-react.js
@@ -0,0 +1,16 @@
+let handler = async(m, { conn, text }) => {
+  if(!text) throw `emotnya mana?`
+  if(!m.quoted) throw `balas pesannya!`
+  conn.relayMessage(m.chat, { reactionMessage: {
+key: {
+ id: m.quoted.id,
+ remoteJid: m.chat,
+ fromMe: true
+ },
+ text: text}}, { messageId: m.id })
+}
+handler.help = ['reaction <reply>']
+handler.tags = ['fun']
+handler.command = /^(react|reaction|r)$/i
+
+export default handler
