28610cd43ca33b8907846baba2bdcde83a6a1a98
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-01 14:24:41 +0700

    Update downloader-joox.js

--- plugins/downloader-joox.js
+++ plugins/downloader-joox.js
@@ -15,8 +15,7 @@ Ukuran File: ${json.result.filesize}
 Durasi: ${json.result.duration}
 `.trim()
     conn.sendFile(m.chat, json.result.thumb, 'eror.jpg', pesan, m)
-    conn.sendFile(m.chat, json.result.mp3_url, 'error.mp3', '', m, 0, { asDocument: db.data.chats[m.chat].useDocument, mimetype: 'audio/mpeg' })
-}
+    conn.sendMessage(m.chat, { document: {json.result.mp3_url}, mimetype: 'audio/mpeg', fileName: `${json.result.judul}.mp3`}, {quoted: m})
 handler.help = ['joox'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
 handler.command = /^joox$/i
