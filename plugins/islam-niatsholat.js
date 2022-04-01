fdcf94139b0045c5a62064f23e019783251ddf92
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-01 16:56:25 +0700

    Update islam-niatsholat.js

--- plugins/islam-niatsholat.js
+++ plugins/islam-niatsholat.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, text }) => {
     let res = await axios.get(API('lolhuman', '/api/niatsholat', { text }, 'apikey'))
+    if (!text) throw 'Masukkan nama sholat\n\nContoh: .niat subuh'
     let json = res.data
     m.reply(`
 ${json.result.name}
