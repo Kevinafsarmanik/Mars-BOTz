c1a5e262cd314a047b88708c2f43dac5d31cc5d9
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 19:29:37 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,6 +1,6 @@
 import { tiktokdl, tiktokdlv2 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedprefix, command }) => {
-    if (!args[0]) throw `use example ${usedprefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
     const url = video.no_watermark_raw || video.no_watermark || video.no_watermark_hd || video.with_watermark 
     if (!url) throw 'can\'t download video!'
@@ -11,7 +11,8 @@ let handler = async (m, { conn, args, usedprefix, command }) => {
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
 handler.command = /^(tiktok|tt|tiktoknowm)$/i
+
+handler.limit = true
 
 export default handler 
