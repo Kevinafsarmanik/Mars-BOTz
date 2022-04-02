52ab0c64cdd994af23fd3a4baa84dd515ef76a35
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 18:05:20 +0700

    Update downloader-joox.js

--- plugins/downloader-joox.js
+++ plugins/downloader-joox.js
@@ -2,21 +2,21 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} akad`
+    if (isUrl(text)) throw `uhm.. judul kak bukan pake url\n\ncontoh:\n${usedPrefix + command} akad`
 
-    let res = await fetch(API('xteam', '/dl/jooxdl', { lagu: text }, 'APIKEY'))
+    let res = await fetch(API('lolhuman', '/api/jooxplay', { query: text }, 'apikey'))
     let json = await res.json()
     if (res.status !== 200) throw await res.text()
     if (!json.status) throw json
-    let { album, album_url, download_url, singers, songname, filesize, duration } = json.result
     let pesan = `
-❖ Title : *${songname}*
-❖ Artists : *${singers}*
-❖ Album : *${album}*
-❖ Duration : *${duration}*
-❖ Size : *${filesize}*
+❖ Title : *${json.result.info.song}*
+❖ Artists : *${json.result.info.singer}*
+❖ Album : *${json.result.info.album}*
+❖ Duration : *${json.result.info.duration}*
+❖ Date : *${json.result.info.date}*
 `.trim()
-    conn.sendFile(m.chat, album_url, 'eror.jpg', pesan, m)
-    conn.sendMessage(m.chat, { document: { url: download_url}, mimetype: 'audio/mpeg', fileName: `${songname}.mp3`}, {quoted: m})
+    conn.sendFile(m.chat, json.result.image, 'eror.jpg', pesan, m)
+    conn.sendMessage(m.chat, { document: { url: json.result.audio[0].link}, mimetype: 'audio/mpeg', fileName: `${json.result.info.song}.mp3`}, {quoted: m})
 }
 handler.help = ['joox'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
@@ -25,3 +25,7 @@ handler.command = /^joox$/i
 handler.limit = true
 
 export default handler
+
+const isUrl = (text) => {
+    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
+}
