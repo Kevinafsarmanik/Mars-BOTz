ff331dc35691676fcc5215d9378106116cf4a09c
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 19:00:05 +0700

    Update sticker-ttp.js

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -2,7 +2,7 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+    let stiker = await sticker(null, `https://api.lolhuman.xyz/api/ttp?apikey=rey2k22&text=${text}`, global.packname, global.author)
     if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     throw stiker.toString()
 }
