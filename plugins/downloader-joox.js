0956d67f955785e582b6ba19b0415fad924e6ab3
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 08:04:21 +0700

    Update downloader-joox.js

--- plugins/downloader-joox.js
+++ plugins/downloader-joox.js
@@ -7,16 +7,15 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let json = await res.json()
     if (res.status !== 200) throw await res.text()
     if (!json.status) throw json
-    let { song, singer, album, date, duration, link } = json.result
     let pesan = `
-❖ Title: *${result.info.song}*
-❖ Artists: *${result.info.singer}*
-❖ Album: *${result.info.album}*
-❖ Duration: *${result.info.duration}*
-❖ Uploaded: *${result.info.date}*
+❖ Title: *${json.result.info.song}*
+❖ Artists: *${json.result.info.singer}*
+❖ Album: *${json.result.info.album}*
+❖ Duration: *${json.result.info.duration}*
+❖ Uploaded: *${json.result.info.date}*
 `.trim()
     conn.sendFile(m.chat, json.result.image, 'eror.jpg', pesan, m)
-    conn.sendMessage(m.chat, { document: { url: result.audio.link}, mimetype: 'audio/mpeg', fileName: `${result.info.song}.mp3`}, {quoted: m})
+    conn.sendMessage(m.chat, { document: { url: json.result.audio.link}, mimetype: 'audio/mpeg', fileName: `${json.result.info.song}.mp3`}, {quoted: m})
 }
 handler.help = ['joox'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
