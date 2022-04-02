609b980ca857817d9ef6c93696aee03fdb655a9d
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 19:23:10 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
-    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : 'teksnya mana?'
-    let sel = API('lolhuman', '/api/attp', { file: '', text: teks })
-    conn.sendFile(m.chat, sel, 'attp.webp', m, 0, {packname: packname, author: author})
+let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : 'teksnya mana?'
+let res = global.API('lolhuman', '/api/attp', { file: '', text: teks })
+ conn.sendFile(m.chat, res, 'attp.webp', m, 0, { packname: global.packname, author: global.author })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
