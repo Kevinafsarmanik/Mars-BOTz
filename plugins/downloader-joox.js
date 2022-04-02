4ab1007daa37f550e063d9475d57b664449c4804
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-02 07:54:00 +0700

    Update downloader-joox.js

--- plugins/downloader-joox.js
+++ plugins/downloader-joox.js
@@ -3,19 +3,19 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Pengunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} akad`
 
-    let res = await fetch(API('chipa', '/api/download/jooxdl', { title: text }, 'apikey'))
+    let res = await fetch(API('lolhuman', '/api/jooxplay', { query: text }, 'apikey'))
     let json = await res.json()
     if (res.status !== 200) throw await res.text()
     if (!json.status) throw json
     let pesan = `
-Judul: ${json.result.judul}
-Artis: ${json.result.artist}
-Album: ${json.result.album}
-Ukuran File: ${json.result.filesize}
-Durasi: ${json.result.duration}
+❖ Title: *${json.result.song}*
+❖ Artists: *${json.result.singer}*
+❖ Album: *${json.result.album}*
+❖ Duration: *${json.result.duration}*
+❖ Uploaded: *${json.result.date}*
 `.trim()
-    conn.sendFile(m.chat, json.result.thumb, 'eror.jpg', pesan, m)
-    conn.sendMessage(m.chat, { document: { url: json.result.mp3_url}, mimetype: 'audio/mpeg', fileName: `${json.result.judul}.mp3`}, {quoted: m})
+    conn.sendFile(m.chat, json.result.image, 'eror.jpg', pesan, m)
+    conn.sendMessage(m.chat, { document: { url: json.result.link}, mimetype: 'audio/mpeg', fileName: `${json.result.song}.mp3`}, {quoted: m})
 }
 handler.help = ['joox'].map(v => v + ' <judul>')
 handler.tags = ['downloader']
