678579c96df95d17cd8e022b8e4ecb66c2b9ca62
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-03 22:38:36 +0700

    Update downloader-igs.js

--- plugins/downloader-igs.js
+++ plugins/downloader-igs.js
@@ -2,17 +2,22 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} instagram`
   if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah\n\ncontoh: *${usedPrefix}${command} instagram*`
+  
   let res = await fetch(API('hardianto', '/api/download/igstory', {
     username: args[0]
   }, 'apikey'))
-  var anu = await res.json()
-  var anuku = anu.medias
+  let anu = await res.json()
+  let anuku = anu.medias
   for (let { url, fileType } of anuku) 
-  conn.sendFile(m.chat, url, 'ig' + (fileType == 'video' ? '.mp4' : '.jpg'), 'nih', m)
+  await delay(1500)
+  conn.sendFile(m.chat, url, 'ig' + (fileType == 'video' ? '.mp4' : '.jpg'), 'Nih banh', m)
 }
 handler.help = ['igstory'].map(v => v + ' <username>')
 handler.tags = ['downloader']
-
 handler.command = /^(igs(tory)?)$/i
 
+handler.limit = true
+
 export default handler
+
+const delay = time => new Promise(res => setTimeout(res, time))
