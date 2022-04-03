f0035f4c6fc72aeb4055da297db799671e0b8fee
TeamMars <94729310+TeamMars20@users.noreply.github.com>
2022-04-03 22:19:24 +0700

    Update downloader-igs.js

--- plugins/downloader-igs.js
+++ plugins/downloader-igs.js
@@ -1,27 +1,14 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/igs', {
-    nama: args[0]
-  }, 'APIKEY'))
-  let json = await res.json()
-  if (res.status != 200) throw json
-  if (json.result.error) throw json.result.message
-  let { username, storylist } = json.result
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  for (let { url, type, taken_at } of storylist)
-    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), `
-@${username}
-Memposting pada ${new Date(taken_at * 1000).toLocaleDateString('id', dateConfig)}
-`, m)
-  throw json.result
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `uhm.. username nya mana?\n\ncontoh:\n\n${usedPrefix + command} instagram`
+  if (args[0].startsWith('http') || args[0].startsWith('@')) throw `username salah\n\ncontoh: *${usedPrefix}${command} instagram*`
+  let res = await fetch(API('hardianto', '/api/download/igstory', {
+    username: args[0]
+  }, 'apikey'))
+  var anu = await res.json()
+  var anuku = anu.medias
+  for (let { url } of anuku) 
+  conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), 'nih', m)
 }
 handler.help = ['igstory'].map(v => v + ' <username>')
 handler.tags = ['downloader']
